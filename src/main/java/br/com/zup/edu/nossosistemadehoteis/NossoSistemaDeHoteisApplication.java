0db1c3a637f37c8e8e4f8c3efad96933a34bd576
jordsilvazup <jordi.silva@zup.com.br>
2022-04-04 09:12:16 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/nossosistemadehoteis/NossoSistemaDeHoteisApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.nossosistemadehoteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoSistemaDeHoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoSistemaDeHoteisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/nossosistemadehoteis/NossoSistemaDeHoteisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.nossosistemadehoteis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoSistemaDeHoteisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
